c48063f43e40c1c861c7092696f380373636d4f0
haris akbar <harisakbar04@gmail.com>
2021-03-24 22:27:04 +0700

    fix jwt token expired

--- utils/checkAuth.js
+++ utils/checkAuth.js
@@ -9,15 +9,19 @@ const checkAuth = (context) => {
   let token
   if (context.req && context.req.headers.authorization) {
     token = context.req.headers.authorization.split("Bearer ")[1]
-    console.log("masuk req")
+    // console.log("masuk req")
   } else if (context.connection && context.connection.context.Authorization) {
-    console.log("masuk connect")
+    // console.log("masuk connect")
     token = context.connection.context.Authorization.split("Bearer ")[1]
   }
   if (token) {
     jwt.verify(token, SECRET_KEY, (err, decodedToken) => {
       if (err) {
+        console.log(err.message)
         throw new AuthenticationError("Unaunthecated")
+      }
+      if (Date.now() >= decodedToken.exp * 1000) {
+        localStorage.removeItem("jwtToken")
       }
       context.decodedToken = decodedToken
     })
