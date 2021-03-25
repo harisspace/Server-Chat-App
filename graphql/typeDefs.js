d6dd2e6a03a22ce03bb87960f86f1cdfa033914a
haris akbar <harisakbar04@gmail.com>
2021-03-25 23:00:35 +0700

    test pagination

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -1,48 +1,48 @@
-const { gql } = require('apollo-server')
+const { gql } = require("apollo-server")
 
 module.exports = gql`
-    type User {
-        username: String!,
-        id: ID!,
-        email: String!,
-        password: String!,
-        token: String,
-        createdAt: String!,
-        updatedAt: String,
-        latestMessage: Message
-    }
+  type User {
+    username: String!
+    id: ID!
+    email: String!
+    password: String!
+    token: String
+    createdAt: String!
+    updatedAt: String
+    latestMessage: Message
+  }
 
-    input RegisterInput {
-        username: String!,
-        password: String!,
-        confirmPassword: String!,
-        email: String!
-    }
+  input RegisterInput {
+    username: String!
+    password: String!
+    confirmPassword: String!
+    email: String!
+  }
 
-    type Message {
-        id: ID!,
-        from: String!,
-        to: ID!,
-        createdAt: String!
-        body: String!
-    }
+  type Message {
+    id: ID!
+    from: String!
+    to: ID!
+    createdAt: String!
+    body: String!
+  }
 
-    type Subscription {
-        newMessage: Message
-    }
+  type Subscription {
+    newMessage: Message
+  }
 
-    type Query {
-        "return all of user but not user login"
-        getUsers: [User]!,
-        "get all messages of user and destination"
-        getMessages(userId: ID!, to: ID!): [Message]!
-        "get all otheruser that user already send message"
-        getUsersMessage(userId: ID!): [User]!
-    }
+  type Query {
+    "return all of user but not user login"
+    getUsers: [User]!
+    "get all messages of user and destination"
+    getMessages(userId: ID!, to: ID!): [Message]!
+    "get all otheruser that user already send message"
+    getUsersMessage(userId: ID!): [User]!
+  }
 
-    type Mutation {
-        register(registerInput: RegisterInput): User!
-        login(username: String!, password: String!): User!
-        sendMessage(from: ID!, to: ID!, body: String!): Message!
-    }
+  type Mutation {
+    register(registerInput: RegisterInput): User!
+    login(username: String!, password: String!): User!
+    sendMessage(from: ID!, to: ID!, body: String!): Message!
+  }
 `
